2764ecfa04869487f5ed6b8f745ec96364b9d9f3
manishpdr <manishpdr@gmail.com>
2024-11-24 15:24:09 +0530

    code updated

--- src/msalConfig.ts
+++ src/msalConfig.ts
@@ -4,7 +4,7 @@ const msalConfig = {
   auth: {
     clientId: 'f333d102-c9c9-4434-9f07-635eecff69a6',  // Replace with your Azure AD app's Client ID
     authority: 'https://login.microsoftonline.com/00e0add0-dc68-40a9-a6a1-019cbcf740b8',  // Replace with your Azure AD Tenant ID
-    redirectUri: 'http://localhost:3000',  // URI where the app redirects after login
+    redirectUri: 'https://mintyapp.azurewebsites.net',  // URI where the app redirects after login
   },
   cache: {
     cacheLocation: 'sessionStorage',  // 'localStorage' if you need persistent sessions

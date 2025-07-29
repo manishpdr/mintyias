2c9a0793648a73dd07f90274d628d34e537faaf0
manishpdr <manishpdr@gmail.com>
2025-07-29 22:07:01 +0530

    updatedcode

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -4,12 +4,13 @@
 import axios from "axios";
 import { PublicClientApplication } from "@azure/msal-browser";
 import msalConfig from "../msalConfig";
+import { useMsal } from "@azure/msal-react";
 
 // Create MSAL instance
 const msalInstance = new PublicClientApplication(msalConfig);
 msalInstance.initialize();
 
-
+  const { instance, accounts } = useMsal();
 // Create Axios instance
 const axiosInstance = axios.create({
   baseURL: "https://mintyapi-a6euhmhxe4dme7du.canadacentral-01.azurewebsites.net", // change to your API base URL
@@ -19,8 +20,15 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   async (config) => {
       try {
-          const token = await msalInstance.acquireTokenSilent({ scopes: ["api://5254975b-cbe7-41a9-9879-e42593962bb5/access-as-user"] });
-        config.headers.Authorization = `Bearer ${token.accessToken}`;
+           msalInstance.acquireTokenSilent({
+          account: accounts[0],
+          scopes: ["api://5254975b-cbe7-41a9-9879-e42593962bb5/access-as-user"],
+        })
+        .then((response) => {
+           config.headers.Authorization = `Bearer ${response.accessToken}`;
+          console.log("Bearer Token: ", response.accessToken);
+        });
+       
       
       } catch (error) {
         console.error("Token acquisition failed", error);
